a3d0d1eb79407675515b66168cf15024444b335e
Ansh Kaushal <ansh1043.be21@chitkarauniversity.edu.in>
2023-04-08 12:13:09 +0530

    bruh

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Made with ❤️ by Anshh Kaushal™</p>
+        <p>Copyright © 2023</p>
+        <br />
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
